c3c1cf24f6af7cfa4840cc4c215a8fb69e9ffd3b
bryanparmelee <bryan.parmelee@gmail.com>
2023-12-15 20:49:50 -0500

    Exercise 8.19

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,14 +1,32 @@
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
+import { useState, useEffect } from "react";
 
 const Books = (props) => {
+  const [genreFilter, setGenreFilter] = useState("all genres");
+  const [filteredBooks, setFilteredBooks] = useState([]);
+
   const result = useQuery(ALL_BOOKS);
+
+  let books = [];
+
+  useEffect(() => {
+    setFilteredBooks(
+      books.filter((book) => book.genres.includes(`${genreFilter}`))
+    );
+  }, [genreFilter]);
 
   if (!props.show) {
     return null;
   }
 
-  const books = [...result.data.allBooks];
+  if (result.loading) {
+    return <div>Loading...</div>;
+  }
+
+  books = [...result.data.allBooks];
+  let genres = new Set().add("all genres");
+  books.map((book) => book.genres.map((genre) => genres.add(genre)));
 
   return (
     <div>
@@ -21,15 +39,28 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {filteredBooks.length
+            ? filteredBooks.map((b) => (
+                <tr key={b.title}>
+                  <td>{b.title}</td>
+                  <td>{b.author.name}</td>
+                  <td>{b.published}</td>
+                </tr>
+              ))
+            : books.map((b) => (
+                <tr key={b.title}>
+                  <td>{b.title}</td>
+                  <td>{b.author.name}</td>
+                  <td>{b.published}</td>
+                </tr>
+              ))}
         </tbody>
       </table>
+      {Array.from(genres).map((genre) => (
+        <button key={genre} onClick={() => setGenreFilter(`${genre}`)}>
+          {genre}
+        </button>
+      ))}
     </div>
   );
 };
